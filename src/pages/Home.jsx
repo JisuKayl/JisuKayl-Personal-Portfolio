6f32e6a3768a59441b7a71d794f7ba038e0a85ff
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2025-03-11 20:53:09 +0800

    fix: remove scroll for profile section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,12 @@ import ContentSection from "../components/ContentSection";
 
 const Home = () => {
   return (
-    <div>
-      <div className="min-h-screen bg-black flex gap-6 p-8 ">
-        <div className="w-1/4 p-4  rounded-xl bg-[#2a2a2a] border-[#444444] border-2">
-          <ProfileSection />
-        </div>
-        <div className="w-3/4 p-4 rounded-xl bg-[#2a2a2a] border-[#444444] border-2">
-          <ContentSection />
-        </div>
+    <div className="h-screen bg-black flex gap-6 p-8 overflow-hidden">
+      <div className="w-1/4 h-full p-4 rounded-xl bg-[#2a2a2a] border-[#444444] border-2">
+        <ProfileSection />
+      </div>
+      <div className="w-3/4 h-full overflow-y-auto p-4 rounded-xl bg-[#2a2a2a] border-[#444444] border-2">
+        <ContentSection />
       </div>
     </div>
   );
